fd9bda28740f688bc81b386b168c0ac9451361ba
kunalg3 <110518293+kunalg3@users.noreply.github.com>
2023-08-20 17:06:33 +0530

    2final

---
+++ Desktop/pwskills_ass/asgn2/assignment_2.py
@@ -0,0 +1,63 @@
+#ans 1
+ Comments in Python are identified with a hash symbol, #, and extend to the end of the line. Hash characters in a  string are not considered comments, however. There are three ways to write a comment - as a separate line, beside the  corresponding statement of code, or as a multi-line comment block.
+ There are three types of comments: single-line, multi-line, and docstring comments. The syntax of comments varies  depending on the type. This tutorial will explore every kind of comment individually, along with examples.
+ 
+ Single-Line Comments
+ Single-line comments begin with the “#” character. Anything that is written in a single line after ‘#’ is considered  as a comment. The syntax for writing single-line comments is:
+ 
+ # comments here
+ Multi-Line Comments
+ Python does not support multi-line comments. However, there are multiple ways to overcome this issue. None of these  ways are technically multi-line comments, but you can use them as one. The first way is by using # at the beginning of  each line of the comment.
+ You can also use multi-line strings for commenting. To do this, use either a ‘’ or “” quotation marks three times.
+ Python Docstrings
+ Python provides an in-built feature called docstrings for commenting on modules, methods, functions, objects, and  classes. They are written in the first line after defining a module, function, method, etc., using three quotation  marks (‘’ or “”). If you do not use it in the first line, the interpreter will not take it as a docstring. You can  also access docstrings using the __doc__ attribute.    
+
+#ans 2
+ Variables are free storage that they can store int,string,bool,double,etc values
+ In Python, variables need not be declared or defined in advance, as is the case in many other programming languages. To create a variable, you just assign it a value and then start using it. Assignment is done with a single equals sign (=)
+ n = 300#example
+
+#Ans 3
+ in python one variable can be converted to another data type as 
+ a="10"
+ b=int(a)
+
+#ans 4
+ The most basic and easy way to run a Python script is by using the python command. You need to open a command line and type the word python followed by the path to your script file like this:
+ python first_script.py
+ Hello World!
+ 
+#ans 5
+ lst=[1,2,3,4,5]
+ print(lst[1:3])
+ 
+#ans 6
+ Complex Numbers in Maths. Complex numbers are the numbers that are expressed in the form of a+ib where, a is real part and b is an imaginary part. and i is iota 
+ # Initializing complex number
+ z = 5+3j
+ # Print the parts of Complex No.
+ print("The real part of complex number is : ", end="")
+ print(z.real)
+ 
+ print("The imaginary part of complex number is : ", end="")
+ print(z.imag)
+
+#ans 7
+ age=25
+
+#ans 8
+ price=9.99
+ print(type(price))
+
+#ans 9
+ name="Kunal Goyal"
+ print(name)
+  
+#ans 10
+ a="Hello World"
+ print(a[6:11])
+
+#ans 11
+ is_student=True
+ print(is_student)
+
